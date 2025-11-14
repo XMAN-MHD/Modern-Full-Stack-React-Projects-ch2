21704532ecb284d73b275bf67bc6af5b957d7968
Tuteur2MD <tuteur2md@gmail.com>
2025-11-14 12:39:34 +0000

    chore: add the recent changes to ch2

---
+++ backend/files.js
@@ -0,0 +1,21 @@
+import { createServer } from 'http'
+import { writeFileSync, readFileSync } from 'fs'
+
+const server = createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'application/json')
+  res.end(readFileSync('backend/users.json'))
+})
+const host = 'localhost'
+const port = 3000
+
+// const users = [{ name: 'Adam Ondra', email: 'adam.ondra@climb.ing' }]
+// const usersJson = JSON.stringify(users)
+// writeFileSync('backend/users.json', usersJson)
+// const readUsersJson = readFileSync('backend/users.json')
+// const readUsers = JSON.parse(readUsersJson)
+// console.log(readUsers)
+
+ server.listen(port, host, () => {
+   console.log(`Server listening on http://${host}:${port}`)
+ })
